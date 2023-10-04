e8028a44fe7d74c181b775550be7b00d207f4cab
clvalada <clvalada@gmail.com>
2023-10-04 15:27:15 -0400

    completed index, mostly complete js, and basic css.

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,183 @@
-//Variables
-var startButtonEl = document.getElementById('#start-button');
-var timeEl = document.getElementById('#timer');
-
-var questions = [
+const questions = [
     {
-        name: "Operators are reserved-words that perform action on values and variables. Which of the following is not an operator?",
-        answers: ["+","===","*","?"],
-        correct: 3,
+        question: "Operators are reserved-words that perform action on values and variables. Which of the following is not an operator?",
+        choices: ["+","===","*","?"],
+        correctAnswer: 3,
     },
     {
-        name: "Which of the following does not create a variable?",
-        answers: ["var","be","let","const"],
-        correct: 1,
+        question: "Which of the following does not create a variable?",
+        choices: ["var","be","let","const"],
+        correctAnswer: 1,
     },
     {
-        name: "From the options below, choose the function you would use to join two arrays.",
-        answers: ["concat()","splice()","add()","push()"],
-        correct: 0,
+        question: "From the options below, choose the function you would use to join two arrays.",
+        choices: ["concat()","splice()","add()","push()"],
+        correctAnswer: 0,
     },
     {
-        name: "What keyword is used to check whether a given property is valid or not?",
-        answers: ["if","then","in","exists"],
-        correct: 2,
+        question: "What keyword is used to check whether a given property is valid or not?",
+        choices: ["if","then","in","exists"],
+        correctAnswer: 2,
     },
     {
-        name: "Which function is use to serialize an objedct into a JSON string in Javascript?",
-        answers: ["serialize()","stringify()","convert()","null()"],
-        correct: 1,
+        question: "Which function is use to serialize an object into a JSON string in Javascript?",
+        choices: ["serialize()","stringify()","convert()","null()"],
+        correctAnswer: 1,
     },
     {
-        name: "How do we write a comment in Javascript?",
-        answers: ["//","<!--comment-->","##","/*/"],
-        correct: 0,
+        question: "How do we write a comment in Javascript?",
+        choices: ["//","<!--comment-->","##","/*/"],
+        correctAnswer: 0,
     }
 ]
 
-//Page displays instructions and start button
+const quizContainer = document.getElementById("quiz");
+const questionContainer = document.getElementById("question");
+const choicesContainer = document.getElementById("choices");
+const resultsContainer = document.getElementById("results");
 
-//User clicks start button
+let currentQuestion = 0;
+let score = 0;
+let quizEnded = false;
+let timerInterval; 
+let timeLeft = 60; 
+let wrongAnswers = 0;
 
-//First question appears and countdown timer begins
+// Start of quiz. Replaces the Start button with the timer and first question
+function startQuiz() {
+    const startButton = document.getElementById("start-button");
+    startButton.style.display = "none";
+    startTimer();
+    displayQuestion();
+}
 
-//User clicks button to answer the question
-
-//If user clicked button === correct answer, display message correct and move to next question
-//else display message incorrect, subtract 10 seconds, and move to next question
-
-//Next question appears
-
-//Game ends when all questions answered or timer ends
-
-startButtonEl.on('onclick', setInterval)
-    function(){
-        (function(){
-        var counter = 5;
-        setInterval(function() {
-            counter--;
-            if (counter >= 0) {
-            span = document.getElementById("count");
-            span.innerHTML = counter;
+// Starts the timer and shows the timer
+function startTimer() {
+    const timerContainer = document.getElementById("timer-container");
+    timerContainer.style.display = "block";
+    timerInterval = setInterval(() => {
+        if (currentQuestion < questions.length) {
+            timeLeft--;
+            if (timeLeft < 0) {
+                timeLeft = 0;
             }
-            if (counter === 0) {
-                alert('sorry, out of time');
-                clearInterval(counter);
+            const timerDisplay = document.getElementById("timer");
+            timerDisplay.textContent = `Time Remaining: ${timeLeft} seconds`;
+            if (timeLeft === 0) {
+                clearInterval(timerInterval); 
+                handleQuizEnd();
             }
-        }, 1000);
-        })();
+        } else {
+            clearInterval(timerInterval);
         }
+    }, 1000);
+}
+    
+
+// Event listener that starts the quiz when the "Start" button is clicked
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", startQuiz);
+
+//Functions for displaying questions
+function displayQuestion() {
+    const currentQuestionObj = questions[currentQuestion];
+    questionContainer.textContent = currentQuestionObj.question;
+    choicesContainer.innerHTML = '';
+
+    // Creates buttons for each choice
+    currentQuestionObj.choices.forEach((choice, index) => {
+        const choiceButton = document.createElement("button");
+        choiceButton.textContent = choice;
+        choiceButton.addEventListener("click", () => {
+            checkAnswer(index); 
+        });
+        choicesContainer.appendChild(choiceButton);
+    });
+}
+
+//Checks the answers as long as time is remaining
+function checkAnswer(answerIndex) {
+    if (timeLeft <= 0) {
+        return;
+    }
+    const currentQuestionObj = questions[currentQuestion];
+    if (answerIndex === currentQuestionObj.correctAnswer) {
+        score++; 
+    } else {
+        wrongAnswers++;
+        timeLeft -= 10;
+    }
+
+    // Move to the next question or finish the quiz
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+        displayQuestion();
+    } else {
+        displayResults();
+    }
+}
+
+// End of Quiz. Stop timer. Alert if time expired. Display results. Ask for intials
+function handleQuizEnd() {
+    quizEnded = true;
+    clearInterval(timerInterval);
+    if (timeLeft <= 0) {
+        alert("Time's up! Your quiz has ended.");
+    } else {
+        const initialsContainer = document.getElementById("initials-container");
+        initialsContainer.style.display = "block";
+        const resultsContainer = document.getElementById("results-container");
+        resultsContainer.style.display = "block";
+    }
+}
+
+function displayResults() {
+    // Hide the question and choices containers
+    questionContainer.style.display = "none";
+    choicesContainer.style.display = "none";
+
+    // Show the initials input field and save button
+    const initialsContainer = document.getElementById("initials-container");
+    initialsContainer.style.display = "block";
+
+    // Display the user's score
+    resultsContainer.textContent = `You scored ${score} out of ${questions.length} questions.`;
+    resultsContainer.style.display = "block";
+}
+
+//Save user initials and score
+const saveButton = document.getElementById("save-initials");
+saveButton.addEventListener("click", () => {
+    const initialsInput = document.getElementById("initials");
+    const initials = initialsInput.value;
+
+    if (initials.trim() !== "") {
+        const userScore = {
+            initials: initials,
+            score: score
+        };
+
+        // Check for existing scores
+        const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+        // Add the user's score to the high scores array
+        highScores.push(userScore);
+
+        // Sort the high scores array in descending order (highest to lowest)
+        highScores.sort((a, b) => b.score - a.score);
+
+        // Keep only the top 5 high scores
+        const topScores = highScores.slice(0, 5);
+
+        // Save the updated high scores back to local storage
+        localStorage.setItem("highScores", JSON.stringify(topScores));
+
+        // Displays a confirmation message
+        alert(`Score saved successfully!`);
+    } else {
+        // Handle empty input or validation as needed
+        alert("Please enter your initials.");
+    }
+});
+
+
